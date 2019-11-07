a877bb89a79826375e87b79b467637276fe5c3fa
masensyons <miguel.galan.tic1@gmail.com>
2019-11-07 18:48:53 +0100

    No se

--- functions.js
+++ functions.js
@@ -3,22 +3,29 @@ function init(){
     botonEnvio = document.querySelector('[type="button"]');
     nuevoItem = document.querySelector('[type="text"]');
     listaCompra = document.getElementById("listaCompra");
-
+    botonEnvio.disabled=true;
     botonEnvio.addEventListener("click",anadir);
 }
 
-function anadir(e){
-    evento = e || window.event;
-    if (nuevoItem.value == ""){
-        evento.preventDefault();
-    }else{
-        var lista = document.createElement("li");
-        lista.innerHTML = nuevoItem.value;
-        lista.addEventListener("dblclick",eliminarLi);
-        listaCompra.appendChild(lista);
-        nuevoItem.value = "";
-    }
+function anadir(){
+    var lista = document.createElement("li");
+    lista.innerHTML = nuevoItem.value;
+    lista.addEventListener("dblclick",eliminarLi);
+    listaCompra.appendChild(lista);
+    nuevoItem.value = "";
 }
 function eliminarLi(){
     this.parentNode.removeChild(this);
+}
+
+function es_vacio()
+{
+    if (nuevoItem.value=="")
+    {
+        botonEnvio.disabled=true;
+    }
+    else
+    {
+        botonEnvio.disabled=false;
+    }
 }
